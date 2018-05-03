86cdabc09d70451ddfcca3bf8176d140d11bd980
Jon Leverkuhn <JonLeverkuhn@gmail.com>
2018-05-03 12:54:49 -0600

    Add email intent code.

--- app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.singlescreenapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+    }
+
+    public void emailIntent(View view) {
+        String[] emailTo = {"GiveUsYourMoney@hotmail.com"};
+
+        Intent sendEmail = new Intent(Intent.ACTION_SENDTO);
+        sendEmail.setData(Uri.parse("mailto:"));
+        sendEmail.putExtra(Intent.EXTRA_EMAIL,emailTo);
+        sendEmail.putExtra(Intent.EXTRA_SUBJECT, "I would love to give you boat loads of money!");
+        if (sendEmail.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendEmail);
+        } else {
+            Toast.makeText(this,"Could not launch email app.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void phoneIntent(View view) {
+    }
+
+    public void mapIntent(View view) {
     }
 }
